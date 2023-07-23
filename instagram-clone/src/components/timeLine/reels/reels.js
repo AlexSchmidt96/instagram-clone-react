f8829cbd33ce8c32db9e4a1676b5e9c08054024a
Alex Schmidt <sa-sch12@mail.ru>
2023-07-23 21:15:53 +0300

    added flex in reels and make them column

--- instagram-clone/src/components/timeLine/reels/reels.js
+++ instagram-clone/src/components/timeLine/reels/reels.js
@@ -5,15 +5,11 @@ import { Avatar } from "@mui/material";
 function Reels({ user }) {
     return (
         <div className="reels">
-            <div className="reels-avatars">
-                <div className="reels-avatar">
-                    <div className="avatar-reel">
-                        <Avatar>{user.charAt(0).toUpperCase()}</Avatar>
-                    </div>
-                    <div className="username-reels">
-                        <span className="username">{user}</span>
-                    </div>
-                </div>
+            <div className="avatar-reel">
+                <Avatar>{user.charAt(0).toUpperCase()}</Avatar>
+            </div>
+            <div className="username-reels">
+                <span className="username">{user}</span>
             </div>
         </div>
     )
